27e514a7b36b4b5fe412304d422e0fad8ae28e0c
sanugrah <sweetpineappleforest@gmail.com>
2015-03-08 16:51:27 +0700

    commit all

---
+++ Login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html> 
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<title>Log In - iMath</title>
+		<link rel="stylesheet" href="iMath.css">
+		<link rel="stylesheet" type="text/css" href="login.css" /> 
+	</head>
+	
+	<body>	
+		<header id = "header">		
+			<img src = "images/Logo.png" height = "50" alt = "iMath">
+			DAFTAR/LOGIN
+		</header>
+		<!--========================================ISI HOME==============================================-->
+		<div id = "sky">
+			
+			<div id = "loginBox">
+			
+				<form>
+					<input class="email-input" placeholder="Email" type="text" value="" name="email" id="email">
+					<input class="password-input" placeholder="Password" type="text" value="" name="password" id="password">
+				</form>			
+				
+			</div>
+		</div>
+		<!--Footer-->		
+	</body>
+</html>
