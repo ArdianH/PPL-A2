30d333477722f416759775778a46624a6536344d
sanugrah <sweetpineappleforest@gmail.com>
2015-03-30 23:09:48 +0700

    new use cases added: baca pesan, hapus pesan, lihat profil, ubah profil

---
+++ contohci/application/views/pesan_view.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+	<title>Daftar Pesan</title>
+    </head>
+    <body>    
+    <h1> Pesan </h1>
+    <table>
+	<thead>
+		<tr>
+			<th>Email</th>			
+			<th>Pesan</th>
+			<th>Operasi</th>
+		</tr>
+	</thead>
+	<?php foreach($result as $row):?>	
+	<tr>
+	<td>
+	 <?php echo $row->email?>
+	</td>
+	<!-- Done Button-->
+	<td>
+	<p><?php echo $row->tanggal?></p>
+	<?php echo $row->isi?> 
+	</td>
+	<td>
+	<!-- Hapus Button-->
+	<a href="<?php echo base_url() ?>/index.php/pesan/delete/<?php echo $row->idPesan?>">
+                                    <button type="submit">Hapus</button></a>
+	</td>
+        <tr>
+        <?php endforeach; ?>
+	</table>
+	</body>
+</html>
