a5e4de2b18289795fd86e854beee189cd7efa2ee
Alivia <alivia.batuparan@gmail.com>
2015-03-31 08:29:11 +0700

    daftar materi utk admin

---
+++ contohci/application/views/daftarmateriview.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+	<title>MATERI</title>
+    </head>
+    
+    <body>
+     <a href=" <?php echo base_url();?>index.php/matericontroller/createview"><button> Buat Baru</button></a>
+
+    <form method="POST" action="<?php echo base_url();?>index.php/matericontroller/viewMateri">
+    <select name ="idKelas">
+	<?php foreach($Kelas as $row):?>			
+	<option value="<?php echo $row->idKelas ?>" ><?php echo $row->idKelas ?></option>
+	<?php endforeach?>
+	</select>
+	<input type="submit" value="Submit" />
+	</form>
+
+    <h1> Daftar Materi </h1>
+    <table>
+	<?php foreach($result as $row):?>	
+	<td>
+		<?php echo $row->idMateri ?>
+	</td>
+	<td>
+		<?php echo $row->nama ?>
+	</td>
+	<td>
+		<?php echo $row->deskripsi ?>
+	</td>
+	<td>
+		<?php echo $row->rangkuman ?>
+	</td>
+	<td>
+	<a href="<?php echo base_url();?>index.php/matericontroller/detail/<?php echo $row->idMateri ?>">
+                                    <button type="submit">Lihat</button></a>
+	</td>
+	<td>
+	<!-- Edit Button-->
+	<a href="<?php echo base_url();?>index.php/matericontroller/edit/<?php echo $row->idMateri ?>">
+                                    <button type="submit">Edit</button></a>
+	</td>
+	<td>
+	<!-- Hapus Button-->
+	<a href="<?php echo base_url();?>index.php/matericontroller/delete/<?php echo $row->idMateri ?>">
+                                    <button type="submit">Hapus</button></a>
+	</td>
+        </tr>
+    <?php endforeach; ?>
+	</table>
+
+    </body>
+</html>
