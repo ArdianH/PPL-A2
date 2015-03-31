eb5505abfc618dda81ce702a403f99126994b0c1
Alivia <alivia.batuparan@gmail.com>
2015-03-31 08:14:27 +0700

    yang masih error bagian upload gambar (baru bisa upload belum bisa store link di db) sama melihat detil materi

---
+++ contohci/application/controllers/matericontroller.php
@@ -0,0 +1,114 @@
+<?php
+class matericontroller extends CI_Controller{
+	public function index()
+	{
+		$this->view(0);
+	}
+
+	public function view($id){
+		$this->load->model('Kelas');          
+	    $data['Kelas'] = $this->Kelas->getAllKelas();
+	    $this->load->model('materimodel');
+		$data['result'] = $this->materimodel->getAllMateri($id);
+	    $this->load->view('daftarmateriview',$data);
+	}
+	public function viewMateri(){
+		$this->load->model('Kelas');          
+	    $data['Kelas'] = $this->Kelas->getAllKelas(); 
+		$this->load->model('materimodel');
+		$id = $this->input->post('idKelas');
+		$data['result'] = $this->materimodel->getAllMateri($id);
+		$this->load->view('daftarmateriview',$data);
+	}
+
+	public function getList($id){
+		$this->load->model('materimodel');		
+
+		$this->materimodel->update($data, $id);
+		redirect('matericontroller', 'refresh');
+	}
+
+	public function delete($id){
+		$this->load->model('materimodel');		
+		
+		$this->materimodel->delete($id);
+		redirect('matericontroller', 'refresh');
+	}
+
+	public function detail($id){
+		$this->load->view('detailmateriview');
+		$this->load->model('materimodel');		
+		
+		$data['result'] = $this->materimodel->get($id); 
+		$this->load->view('detailmateriview', $data);
+	}	
+	
+	public function edit($id){
+		$this->load->model('materimodel');		
+		
+		$data['result'] = $this->materimodel->get($id); 
+		$this->load->view('ubahmateriview', $data);
+	}
+	
+	public function simpanPerubahan($id){
+		$this->load->model('materimodel');		
+		
+		$data = array(			
+			'idKelas' => $this->input->post('idKelas'),
+			'nama' => $this->input->post('nama'),
+			'deskripsi' => $this->input->post('deskripsi'),
+			'rangkuman' => $this->input->post('rangkuman')			
+		);
+		
+		$this->materimodel->update($data, $id);
+		redirect('matericontroller', 'refresh');
+	}
+
+	public function createview(){
+		$this->load->model('Kelas');          
+	    $data['Kelas'] = $this->Kelas->getAllKelas();
+		$this->load->view('buatmateriview', $data);
+	}
+	
+	public function insert(){
+		$this->load->model('materimodel');
+		$this->materimodel->insert();
+		$this->load->view('daftarmateriview');
+	}
+
+	function create()
+	{
+		$this->load->model('materimodel');
+		$data = array(
+			'nama' => $this->input->post('nama'),
+			'idMateri' => $this->input->post('idMateri'),
+			'idKelas' => $this->input->post('idKelas'),			
+			'rangkuman' => $this->input->post('rangkuman'),
+			'deskripsi' => $this->input->post('deskripsi')
+		);
+		
+		$this->materimodel->add($data);
+		redirect('matericontroller');
+	}
+
+	function do_upload()
+	{
+		$this->load->library('upload');
+		if ( ! $this->upload->do_upload())
+		{
+			$error = array('error' => $this->upload->display_errors());
+		}
+		else
+		{
+			$data = array('upload_data' => $this->upload->data());           
+            // load images model
+            $this->load->model('materimodel');
+            $this->materimodel->insert_image($data);           
+            // insert
+            $upload = $data['upload_data'];
+            //ini link gambarnya tapi masih bingung gimana cara masukinnya ke database bersamaan dengan isi form yang lainnya
+        	$full_path = $upload['full_path'];
+		}
+	}
+}
+?>
