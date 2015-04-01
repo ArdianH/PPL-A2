17468a7b16dc78db3e166474851c32bb8c549fb9
Alivia <alivia.batuparan@gmail.com>
2015-04-01 14:28:37 +0700

    yg udah ada upload gambar

--- contohci/application/views/buatmateriview.php
+++ contohci/application/views/buatmateriview.php
@@ -4,24 +4,23 @@
     </head>
     <body>    
     <h1>Buat Materi </h1> 
-	<form method="POST" action="<?php echo base_url()?>index.php/matericontroller/create"> 	
+	<form method="POST" action="<?php echo base_url()?>index.php/matericontroller/create" enctype="multipart/form-data"> 	
 	<p>nama <input type="text" name ="nama"></p>
 	<p>id <input type="text" name ="idMateri"></p>
+
 	 <p>Kelas <select name ="idKelas">
-	<?php foreach($Kelas as $row):?>			
+	<?php foreach($Kelas as $row): ?>			
 	<option value="<?php echo $row->idKelas ?>" ><?php echo $row->idKelas ?></option>
-	<?php endforeach?>
+	<?php endforeach ?>
 	</select>
 	<p>deskripsi <input type="text" name ="deskripsi"></p>
 	<p>rangkuman <input type="text" name ="rangkuman"></p>	
 	<p>
-		<input type="submit" value="Submit" /></form>
-		<a href = "<?php echo base_url()?>index.php/matericontroller"><button/>Batal</button></a>
 	</p>
-	<?php echo form_open_multipart('matericontroller/do_upload');?>
 	<input type="file" name="userfile" size="20" />
 	<br /><br />
-	<input type="submit" value="upload" /> </form>
+	<input type="submit" value="Submit" /> </form>
+	<a href = "<?php echo base_url()?>index.php/matericontroller"><button/>Batal</button></a>
 	
     </body>
 </html>
--- contohci/application/views/ubahmateriview.php
+++ contohci/application/views/ubahmateriview.php
@@ -6,16 +6,21 @@
     <h1>Ubah Materi </h1> 
     <?php $id=$result[0]->idMateri;
     echo $id;?>
-	<form method="POST" action="<?php echo base_url(); ?>index.php/matericontroller/simpanPerubahan/<?php echo $id; ?>">
-	<p>Nama<input type="text" name ="nama" value="<?php echo $result[0]->nama ;?>"></p>
-	<p>Kelas <input type="text" name ="idKelas"  value="<?php echo $result[0]->idKelas ;?>"></p>
-	<p>Deskripsi <input type="text" name ="deskripsi" value="<?php echo $result[0]->deskripsi ;?>"></p>
-	<p>Rangkuman <input type="text" name ="rangkuman"  value="<?php echo $result[0]->rangkuman ;?>"></p>
+	<form method="POST" action="<?php echo base_url()?>index.php/matericontroller/simpanPerubahan/<?php echo $id ?>" enctype="multipart/form-data"> 	
+	<p>nama <input type="text" name ="nama"></p>
+	<p>id <input type="text" name ="idMateri"></p>
+	 <p>Kelas <select name ="idKelas">
+	<?php foreach($Kelas as $row): ?>			
+	<option value="<?php echo $row->idKelas ?>" ><?php echo $row->idKelas ?></option>
+	<?php endforeach ?>
+	</select>
+	<p>deskripsi <input type="text" name ="deskripsi"></p>
+	<p>rangkuman <input type="text" name ="rangkuman"></p>	
 	<p>
-		<input type="submit" value="Submit" />
 	</p>
-	
-	</form>
+	<input type="file" name="userfile" size="20" />
+	<br /><br />
+	<input type="submit" value="Submit" /> </form>
 	
     </body>
 </html>
