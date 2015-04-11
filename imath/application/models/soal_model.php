fd7bb7bb7014dc33f6bd4757f9f291404540f8e1
Alivia <alivia.batuparan@gmail.com>
2015-04-11 23:41:42 +0700

    soal latihan udh bisa edit delete detail

--- imath/application/models/soal_model.php
+++ imath/application/models/soal_model.php
@@ -14,29 +14,19 @@ class soal_model extends CI_Model {
 	//Fungsi ini mengambil soal dengan id yang diinginkan
 	function get($id){
     	$this->load->database();
-		return $this->db->get_where('soal', array('idsoal' => $id))->result();
+		return $this->db->get_where('soal', array('idSoal' => $id))->result();
+	}
+	function getPilihanJawaban($id){
+    	$this->load->database();
+		return $this->db->get_where('pilihan_jawaban', array('idSoal' => $id))->result();
 	}
 
 	function delete($id){
 		$this->load->database();		
-		$this->db->delete('soal', array('idsoal' => $id));
+		$this->db->delete('soal', array('idSoal' => $id));
+		$this->db->delete('pilihan_jawaban', array('idSoal' => $id));
 	}
-	
-	function insert(){
-		$this->load->database();
-		$isTes = 'false';
-		$this->db->query("insert into soal ( idKelas, idMateri, pertanyaan, jawaban, pembahasan, isTes, gambar) 
-			values('$kelas', '$materi', '$pertanyaan', '$jawaban','$isTes', '$gambar')");
-		$this->db->query("insert into pilihan_jawaban ( idSoal, pilihanGanda, deskripsi, gambarJawaban) 
-			values('$id', 'a', 'optiona', 'gambara')");
-		$this->db->query("insert into pilihan_jawaban ( idSoal, pilihanGanda, deskripsi, gambarJawaban) 
-			values('$id', 'b', 'optionb', 'gambarb')");
-		$this->db->query("insert into pilihan_jawaban ( idSoal, pilihanGanda, deskripsi, gambarJawaban) 
-			values('$id', 'c', 'optionc', 'gambarc')");	
-		$this->db->query("insert into pilihan_jawaban ( idSoal, pilihanGanda, deskripsi, gambarJawaban) 
-			values('$id', 'd', 'optiond', 'gambard')");	
-	}
-	
+		
 	function add($data){
 		$this->db->insert('soal', $data);
 		$a = $this->db->insert_id();
@@ -45,8 +35,13 @@ class soal_model extends CI_Model {
 	
 	function update($data, $id){
 		$this->db->where('idsoal', $id);
-		$this->db->update('soal', $data);
-		return;		
+		$this->db->update('soal', $data);		
+	}
+
+	function updateJawaban($data, $id, $pilihanGanda){
+		$this->db->where('idsoal', $id);
+		$this->db->where('pilihanGanda', $pilihanGanda);
+		$this->db->update('pilihan_jawaban', $data);		
 	}
 }
 ?>
