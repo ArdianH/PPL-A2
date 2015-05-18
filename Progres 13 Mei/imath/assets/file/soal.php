1e9a614bfb31db5a9e54aa204e07c5cde727911a
Ardian <ardian_hosen@live.com>
2015-05-18 13:15:10 +0700

    Ardian: permainan ular tangga added

---
+++ Progres 13 Mei/imath/assets/file/soal.php
@@ -0,0 +1,20 @@
+KPK dari 250 dan 450 adalah 2250
+FPB dari 30, 50, dan 60 adalah   10
+935 + (87 X 19) + (-960 : 16) = 2528
+(7.575 - 5.757):18 + 899 = 1000
+Luas layang-layang yang panjang diagonalnya 30 dm dan 20 dm adalah (dm2)  300
+Sebuah benda berbentuk trapesium dengan sisi-sisi yang sejajar adalah 15 cm dan 20 cm. Tinggi trapesium 8 cm. Luas trapesium tersebut adalah (cm2)  140
+25.200 detik = ... jam    7
+Taksiran kira-kira 6.421 - 2.640 adalah 3000
+Lambang bilangan XXIV adalah   24
+Andi sedang mendaki gunung. Ia berada pada ketinggian 175 m di atas permukaan laut. Kemudian ia naik lagi sejauh 125 m. Andi sekarang berada pada ketinggian  300
+Q X 24 + 18 = 1.146 Nilia Q adalah   47
+Jumlah kelereng Anton 369 butir. Jumlah kelereng Toni 216 butir. Jumlah kelereng mereka adalah  585
+420 + 72 : 3 X 5 =  540
+2.500 : 25 =  100
+Angka yang ditunjuk jarum jam pendek pada pukul 17.00 adalah angka    5
+Seorang atlet menempuh jarak 9 km dalam waktu 1 jam. Untuk menempuh jarak 36 km memerlukan waktu ... menit  240
+4 minggu = ... jam  672
+Panjang diagonal layang-layang masing-masing adalah 14 cm dan 28 cm. Luas layang-layang tersebut adalah  196
+Andi membuat enam potongan kertas berbentuk persegi. Ukuran persegi tersebut adalah 10 cm. Andi merekatkannya menjadi kubus. Volume kubus yang dibuat Andi adalah (cm3) 1000
+Jarak kota A ke kota B di peta 4 cm. Pada peta tertera skala 1:1.000.000. Jarak sesungguhnya adalah ... km   40
